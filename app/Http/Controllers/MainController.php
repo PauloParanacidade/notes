64b130ff8349c8ad9cd8264d28cd068bd58963c3
Paulo <paulo.souza@paranacidade.org.br>
2025-05-12 17:31:42 -0300

    complementando o commit anterior

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -7,16 +7,5 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function index($Value)
-    {
-        return view('main', ['value' => $Value]);
-    }
-     public function page2($Value)
-    {
-        return view('page2', ['value' => $Value]);
-    }
-     public function page3($Value)
-    {
-        return view('page3', ['value' => $Value]);
-    }
+  
 }
