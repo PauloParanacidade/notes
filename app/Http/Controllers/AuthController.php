c080a3201604c483eb0e7b57e6201c3f9f00d172
Paulo <paulo.souza@paranacidade.org.br>
2025-05-14 16:18:44 -0300

    até a criação da base de dados

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
@@ -13,9 +14,43 @@ class AuthController extends Controller
 
     public function loginSubmit (Request $request)// quando o formulário é enviado, o método loginSubmit é chamado
     {
-        echo $request->input('text_username');
-        echo '<br>';
-        echo $request->input('text_password');
+        // form validation
+        $request->validate(
+            //rules
+            [
+                'text_username' => 'required|email',
+                'text_password' => 'required|min:6|max:16'
+            ],
+
+            //error messages
+            [
+                'text_username.required' => 'O nome de usuário é obrigatório!',
+                'text_username.email' => 'O nome de usuário deve ser um e-mail válido!',
+                'text_password.required' => 'A senha é obrigatória!',
+                'text_password.min' => 'A senha deve ter no mínimo :min caracteres',
+                'text_password.max' => 'A senha deve ter no máximo :max caracteres'
+            ]
+        );
+
+        // get user input
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
+
+        //echo 'ok';
+
+        // test database connection
+        try{
+            DB::connection()->getPdo();// é o método que vai fazer o teste de conexão
+            echo 'Connection is OK!';
+        } catch (\PDOException $e){
+            echo "Connection failed: " . $e->getMessage();
+        }
+
+        echo "FIM!";
+
+        //echo $request->input('text_username');
+        //echo '<br>';
+        //echo $request->input('text_password');
         
         //dd($request);
         //dd() é uma função de depuração que exibe o conteúdo de uma variável, array ou objeto e interrompe a execução do 
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -13,17 +13,26 @@
                     <!-- form -->
                     <div class="row justify-content-center">
                         <div class="col-md-10 col-12">
-                            <form action="/loginSubmit" method="post">
+                            <form action="/loginSubmit" method="post" novalidate> {{-- quando o novalidate está aqui nesse campo ele invalida todos os required do html--}}
+                                
                                 @csrf
                                     {{-- só CSRF -> ataque malicioso para roubar dados de autenticação 
                                     Token CSRF -> proteção contra esse tipo de ataque  --}}
                                 <div class="mb-3">
                                     <label for="text_username" class="form-label">Username</label>
-                                    <input type="text" class="form-control bg-dark text-info" name="text_username" required>
+                                    <input type="email" class="form-control bg-dark text-info" name="text_username" value="{{ old('text_username')}}" required>
+                                    {{-- show error --}}
+                                    @error('text_username')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
                                 </div>
                                 <div class="mb-3">
                                     <label for="text_password" class="form-label">Password</label>
-                                    <input type="password" class="form-control bg-dark text-info" name="text_password" required>
+                                    <input type="password" class="form-control bg-dark text-info" name="text_password"  value="{{ old('text_password')}}" required>
+                                    {{-- show error --}}
+                                    @error('text_password')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
                                 </div>
                                 <div class="mb-3">
                                     <button type="submit" class="btn btn-secondary w-100">LOGIN</button>
@@ -36,7 +45,18 @@
                     <div class="text-center text-secondary mt-3">
                         <small>&copy; <?= date('Y') ?> Notes</small>
                     </div>
-
+                    {{-- errors --}}
+                    {{-- com o código abaixo é possível mostrar os erros abaixo do botão do login.
+                    Eles não serão mostrados logo abaixo dos seus respectivos campos, como implantado acima --}}
+                    {{-- @if ($errors->any())
+                        <div class="alert alert-danger mt-3">
+                            <ul class="m-0">
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif    --}}
                 </div>
             </div>
         </div>
